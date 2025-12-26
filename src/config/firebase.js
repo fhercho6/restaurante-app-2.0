275cba84d43cdbf011891060e3c724b7cee5ad64
fer <fernandopozo6@gmail.com>
2025-12-26 00:36:47 -0400

    Fix: Restore storage bucket to sistemazzif.firebasestorage.app

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: 'sistemazzif.appspot.com', // FORCEFIX: Bypass .env issue
+  storageBucket: 'sistemazzif.firebasestorage.app', // Correct bucket from screenshot
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
