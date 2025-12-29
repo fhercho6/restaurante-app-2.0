5b393e4615f2ccdaf3526f65c57cca9e95501e3f
fercho6 <fernandopozo6@gmail.com>
2025-12-29 18:32:24 -0400

    fix: tune barcode density to retail standard (width=1.5) and widen container to 300px to fix scan width overflow

--- src/components/Views.jsx
+++ src/components/Views.jsx
@@ -385,7 +385,7 @@ export const AttendanceTicket = ({ data }) => (
 
 // --- CREDENCIAL PARA IMPRIMIR (COMPACTA) ---
 export const CredentialPrintView = ({ member, appName }) => (
-    <div className="w-[260px] h-[155px] bg-white border border-gray-300 p-3 m-4 flex flex-col rounded-lg shadow-lg relative overflow-hidden print:shadow-none print:border-black print:m-0">
+    <div className="w-[300px] h-[155px] bg-white border border-gray-300 p-3 m-4 flex flex-col rounded-lg shadow-lg relative overflow-hidden print:shadow-none print:border-black print:m-0">
         <div className="absolute top-0 right-0 w-12 h-12 bg-orange-500 transform rotate-45 translate-x-6 -translate-y-6"></div>
 
         {/* TOP SECTION: INFO & PHOTO */}
@@ -410,8 +410,8 @@ export const CredentialPrintView = ({ member, appName }) => (
             <Barcode
                 value={member.id.substring(0, 6).toUpperCase()}
                 format="CODE128"
-                width={2.2}
-                height={40}
+                width={1.5}
+                height={45}
                 displayValue={false}
                 margin={15}
                 background="transparent"
